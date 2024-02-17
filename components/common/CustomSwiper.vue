513e0251621d2b9b0898889a7490e7a1c5417f7f
Mitry <isorrowno@gmail.com>
2024-02-17 14:55:29 -0300

    test

--- components/common/CustomSwiper.vue
+++ components/common/CustomSwiper.vue
@@ -7,6 +7,7 @@
       </p>
     </div>
     <Swiper
+      v-if="sliderOn"
       :modules="[SwiperAutoplay]"
       :slidesPerView="props.maxPerView"
       :simulateTouch="false"
@@ -38,6 +39,12 @@ const props = defineProps({
   bgColor: { type: String, default: "white" },
   solo: { type: Boolean, default: false }
 });
+
+const sliderOn = ref(false);
+
+onMounted(() => {
+  setTimeout(() => { console.log("swiper"); sliderOn.value = true; }, 1000)
+ })
 </script>
 
 <style>
