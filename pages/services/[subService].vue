e2fa2075fcfef1c6103cecf1a4573e2421bb39bc
Mitry <isorrowno@gmail.com>
2024-02-19 22:09:58 -0300

    r

--- pages/services/[subService].vue
+++ pages/services/[subService].vue
@@ -1,6 +1,10 @@
 <template>
   <article class="services">
-    <div v-if="pageData.picture" class="t"></div>
+    <div
+      v-if="pageData.picture"
+      :style="`background-image: url(${pageData.picture});`"
+      class="services__picture"
+    />
     <CommonTitleLine
       :title="pageData.title"
       :centered="true"
@@ -9,16 +13,13 @@
       :size="42"
     />
     <section class="services__wrap">
-      <div class="servicess__content">
-        <div v-html="pageData.text" class="services__text"/>
-        <CommonDynamicButton
-          link="/free-consultation"
-          :text="pageData.buttonText"
-          :isIcon="true"
-          class="services__button"
-        />
-      </div>
-      <img v-if="pageData.textImage" :src="pageData.textImage" alt="">
+      <div v-html="pageData.text" class="services__text"/>
+      <CommonDynamicButton
+        link="/free-consultation"
+        :text="pageData.buttonText"
+        :isIcon="true"
+        class="services__button"
+      />
     </section>
   </article>
 </template>
@@ -28,7 +29,7 @@ const pages = ref({
   "executive-services": {
     title: "Executive Services",
     buttonText: "GET IN TOUCH",
-    textImage: "/images/t1.png",
+    picture: "/images/x2.jpg",
     text: [
       "<p>Keeping pace with technological advancements can be challenging for data professionals, let alone for those steering the business. Our suite of personalized Executive Services is designed for high-value data decisioning and AI strategy. It aims to demystify emerging technologies such as AI, MDM, data science, machine learning, and analytics, providing insights into how these can be leveraged to enhance value within your organization.</p>",
       "<ul class='services__list'>",
@@ -46,7 +47,7 @@ const pages = ref({
     text: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>",
     buttonText: "Drive Your Data Strategy Forward Now",
     meta: {},
-    picture: true
+    picture: "/images/x.jpg",
   },
   services: {
     title: "service title",
@@ -83,7 +84,6 @@ const pages = ref({
 const route = useRoute();
 
 const pageData = computed(() => pages.value[route.params.subService]);
-const textWidth = computed(() => pageData.value.textImage ? "66%" : "100%");
 
 const changeHeader = inject("changeHeader");
 
@@ -91,10 +91,9 @@ onMounted(() => setTimeout(() => changeHeader(true), 100));
 </script>
 
 <style>
-.t {
+.services__picture {
     width: 100%;
     height: 500px;
-    background-image: url("/images/x.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: 100% 10%;
@@ -112,19 +111,9 @@ onMounted(() => setTimeout(() => changeHeader(true), 100));
     width: 100%;
     padding: 20px 60px;
     display: flex;
-    justify-content: space-between;
-    margin-bottom: 60px;
-}
-.servicess__content {
-    max-width: v-bind(textWidth);
-    display: flex;
     flex-direction: column;
     gap: 40px;
-}
-.services__wrap img {
-    max-width: 30%;
-    height: 270px;
-    place-self: center;
+    margin-bottom: 60px;
 }
 .services__text {
     width: 100%;
